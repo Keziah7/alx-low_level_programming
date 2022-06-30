28a95671d1b832e58522fb26afe4dd75d0e32f38
Keziah7 <ongangikeziah@gmail.com>
2022-06-30 12:18:19 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,10 @@
  *
  *   * Return: 0 if success.
  *
-*/
+ */
+
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
+{
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
