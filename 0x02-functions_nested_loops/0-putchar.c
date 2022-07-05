702a11179305dbc085f8c294bb907f555b0afdd6
Keziah7 <ongangikeziah@gmail.com>
2022-07-05 14:26:26 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,19 @@
 #include "main.h"
 /**
- * main - program that prints Holberton.
- * Return: int
+ * main - check description
+ * Description: it prints _putchar, followed by a new line.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int n = 0;
-char str_hbt[9] = "Holberton";
-while (n < 9)
-{
-_putchar(str_hbt[n]);
-n += 1;
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
