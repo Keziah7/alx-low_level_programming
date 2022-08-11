2c6f438db77d7468b5038d95b769d778f8a6ed77
Keziah7 <ongangikeziah@gmail.com>
2022-08-11 10:46:37 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,32 +1,37 @@
 #include "lists.h"
-#include <string.h>
 /**
- * add_node - Adds a new node at the beginning of a list.
- * @head: A pointer to the head of the structure list_t list.
- * @str: string to be added to the structure list_t list.
- * Return: If the function fails - NULL.
- *        Otherwise - Address of the new element.
+ * add_node_end - Add a new node at the end of a list.
+ * @head: Address of the first node of a list.
+ * @str: Address of the string to insert into the new node.
+ * Return: Address of the new node.
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-char *duplicate;
-int length;
-list_t *newptr;
-newptr = malloc(sizeof(list_t));
-if (newptr == NULL)
+list_t *temp, *temp2;
+unsigned int length = 0;
+if (str == NULL)
 return (NULL);
-duplicate = strdup(str);
-if (duplicate == NULL)
+temp = malloc(sizeof(list_t));
+if (temp == NULL)
+return (NULL);
+temp->str = strdup(str);
+if (temp->str == NULL)
 {
-free(newptr);
+free(temp);
 return (NULL);
 }
-length = 0;
 while (str[length])
 length++;
-newptr->str = duplicate;
-newptr->len = length;
-newptr->next = *head;
-*head = newptr;
-return (newptr);
+temp->len = length;
+temp->next = NULL;
+if (*head == NULL)
+{
+*head = temp;
+return (temp);
+}
+temp2 = *head;
+while (temp2->next)
+temp2 = temp2->next;
+temp2->next = temp;
+return (temp);
 }
