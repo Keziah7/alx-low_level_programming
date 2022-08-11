0ea7256675ac4d4dff3c585a3d5ca26d113d4a3f
Keziah7 <ongangikeziah@gmail.com>
2022-08-11 10:17:30 -0700

    #include "lists.h"
    /**
    
     * add_node - add a new node at the beginning of a list
    
     * @head: head of the linked list
    
     * @str: string in the struct
    
     *
    
     * Return: the address of the new element
    
     */
    
    list_t *add_node(list_t **head, const char *str)
    
    {
    
    list_t *newnode;
    
    int newelem = 0;
    
    newnode = malloc(sizeof(list_t));
    
    if (newnode == NULL)
    
    {
    
    return (NULL);
    
    }
    
    newnode->str = strdup(str);
    
    while (str[newelem])
    
    {
    
    newelem++;
    
    }
    
    newnode->len = newelem;
    
    newnode->next = *head;
    
    *head = newnode;
    
    return (*head);
    
    }

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,35 +1,26 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
-#include <string.h>
 /**
- * add_node - function that prints all the elements of a list_t list
- * @head: input header pointer
- * @str: Input string value
- * Return: address of the new element, or NULL if it failed
+ * add_node - add a new node at the beginning of a list
+ * @head: head of the linked list
+ * @str: string in the struct
+ * Return: the address of the new element
  */
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new_node;
-new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
-return (NULL);
-new_node->len = strlen(str);
-new_node->len = _strlen(str);
-new_node->str = strdup(str);
-new_node->next = *head;
-*head = new_node;
-return (new_node);
-}
-/**
- * _strlen - returns length of string
- * @s: character of string
- * Return: length of string
- */
-int _strlen(const char *s)
+list_t *newnode;
+int newelem = 0;
+newnode = malloc(sizeof(list_t));
+if (newnode == NULL)
 {
-int i;
-while (s[i] != 0)
-i++;
-return (i);
+return (NULL);
+}
+newnode->str = strdup(str);
+while (str[newelem])
+{
+newelem++;
+}
+newnode->len = newelem;
+newnode->next = *head;
+*head = newnode;
+return (*head);
 }
